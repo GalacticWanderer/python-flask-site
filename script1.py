dbd54f253be0bc5930ffca813231e3d5620fe2f6
Joy Paul <jpaul3250@gmail.com>
2019-02-14 20:56:09 -0500

    init commit with sctipt1 and templates

---
+++ script1.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template  # import flask
+
+# assigning a name to the flask app. __name__ gets turned into __main__
+# when executed
+app = Flask(__name__)
+
+# the root directory of the site
+@app.route('/')  # accessible at http://localhost:5000/
+# funcs for home page
+def home():
+    # returns a render_template from the templates folder
+    return render_template("home.html")
+
+
+# the home directory of the site
+@app.route('/about')  # accessible at http://localhost:5000/about
+# funcs for about page
+def about():
+    # returns a render_template from the templates folder
+    return render_template("about.html")
+
+
+# running the app with debug mode
+if __name__ == "__main__":
+    app.run(debug=True)
